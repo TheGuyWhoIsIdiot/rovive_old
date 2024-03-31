7c51d6eeb0b10e8d4c566c38ad12d8de2512c662
TheGuyWhoIsIdiot <157973030+TheGuyWhoIsIdiot@users.noreply.github.com>
2024-04-01 00:31:34 +0200

    Add files via upload

---
+++ UploadMedia/UploadVideo.aspx.php
@@ -0,0 +1,8 @@
+<h2 class="font-weight-normal">test Video</h2>
+<p>Hey, you just recorded a video in test! You could:</p>
+<ul>
+	<li>Go to <a href="#" onclick="window.external.OpenVideoFolder();">My Videos</a> folder to check it out!</li>
+	<li><a href="#" onclick="window.external.UploadVideo('', true, 1, ''); window.close();">click for boop</a></li>
+</ul>
+<hr class="divider-bottom">
+<a href="#" onclick="window.external.UploadVideo('', false, 0, ''); window.close();">Not interested, don't bother me again</a>
